9e06dee6efc7e18ce356df37b5065f09d2768f73
Sushant <panditsushant11@gmail.com>
2025-04-22 21:14:07 +0530

    add frontend files

---
+++ merntodolist/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './assets/Home'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
