d917b40167da02130b905e304ce8006ddec548c2
Sushant <panditsushant11@gmail.com>
2025-04-23 17:36:57 +0530

    conect the server to the forntend

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,12 @@ app.use(cors())
 app.use(express.json)
 
 
+
+app.post('/add', (req, res => {
+const task = req.body.task;
+}))
+
+
 app.listen(3001, () =>{
     console.log("Server is Running")
 })
