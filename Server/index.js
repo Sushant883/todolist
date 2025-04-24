6b761f29dacd3cba00663d6d105f0d153e4ea5b4
Sushant <panditsushant11@gmail.com>
2025-04-24 17:29:03 +0530

    connect the frontend and backend server together and add the database

--- Server/index.js
+++ Server/index.js
@@ -10,6 +10,13 @@ app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/todolist");
 
+app.get('/get',(req,res) =>{
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+
 app.post("/add", (req, res) => {
   const task = req.body.task;
   TodoModel.create({
