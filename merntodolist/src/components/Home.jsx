694765436b96bc16d0cc071479af1b6b960bc9d0
Sushant <panditsushant11@gmail.com>
2025-04-24 21:25:14 +0530

    final code

--- merntodolist/src/components/Home.jsx
+++ merntodolist/src/components/Home.jsx
@@ -23,6 +23,15 @@ function Home() {
       .catch((err) => console.log(err));
   };
 
+
+  const handleDelete = (id) =>{
+    axios.delete('http://localhost:3001/delete/'+id)
+    .then(result => {
+      location.reload()
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="home">
       <h2>Todo List</h2>
@@ -47,7 +56,8 @@ function Home() {
 
             <div>
               <span>
-                <BsFillTrashFill className="icon" />
+                <BsFillTrashFill className="icon" 
+                onClick={() => handleDelete(todo._id)}/>
               </span>
             </div>
           </div>
